d738b49c10e728235929fa7ebaf4e7724dd4321e
Andrey Kurbatov <nonamegod@gmail.com>
2022-06-12 05:19:05 +0300

    finish

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,14 +1,33 @@
 const { Contact } = require("../../models");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({});
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      result,
-    },
-  });
+  const { _id } = req.user;
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id email subscription");
+
+  if (favorite) {
+    const showFavorite = await Contact.find({ favorite: true });
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        showFavorite,
+      },
+    });
+  } else {
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result,
+      },
+    });
+  }
 };
 
 module.exports = getAll;
---
+++ routes/api/users.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const { user, validation, ctrlWrapper } = require("../../middlewares");
+const { users: ctrl } = require("../../controllers");
+const {
+  joiSignupSchema,
+  joiLoginSchema,
+  subscriptionJoiSchema,
+} = require("../../models/user");
+
+const router = express.Router();
+
+router.post("/signup", validation(joiSignupSchema), ctrlWrapper(ctrl.signup));
+
+router.post("/login", validation(joiLoginSchema), ctrlWrapper(ctrl.login));
+
+router.post("/logout", user, ctrlWrapper(ctrl.logout));
+
+router.get("/current", user, ctrlWrapper(ctrl.getCurrent));
+
+router.patch(
+  "/:id/subscription",
+  validation(subscriptionJoiSchema),
+  ctrlWrapper(ctrl.updateUserSubscription)
+);
+
+module.exports = router;
